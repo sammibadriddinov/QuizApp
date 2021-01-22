4b746ca3fe96e576cbeb31cc27a2dac518b1631f
sammibadriddinov <76088408+sammibadriddinov@users.noreply.github.com>
2021-01-22 14:20:22 +0500

    Quiz

---
+++ question1.js
@@ -0,0 +1,36 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What are the five colours of the Olympic rings?",
+    answer: "blue, yellow, black, green and red",
+    options: [
+      "blue, yellow, black, green and red",
+      "blue, yellow, black, green and crimson",
+      "blue, yellow, black, green and aqua",
+      "blue, yellow, black, green and white"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many players are there in a rugby league team?",
+    answer: "13",
+    options: [
+      "11",
+      "10",
+      "13",
+      "9"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who won the FIFA Women's World Cup in 2019?",
+    answer: "USA",
+    options: [
+      "England",
+      "Brazilia",
+      "Argentina",
+      "USA"
+    ]
+  },
+];
